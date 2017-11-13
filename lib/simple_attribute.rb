b898b2f44d32370a463dc7ad304a7afbcc51160e
Jonian Guveli <jonian@hardpixel.eu>
2017-11-13 15:13:58 +0200

    update main module to include config and modules

--- lib/simple_attribute.rb
+++ lib/simple_attribute.rb
@@ -1,5 +1,28 @@
+require 'action_view'
 require 'simple_attribute/version'
 
 module SimpleAttribute
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  # Autoload base modules
+  autoload :Config,     'simple_attribute/config'
+  autoload :Builder,    'simple_attribute/builder'
+  autoload :Helpers,    'simple_attribute/helpers'
+  autoload :Attributes, 'simple_attribute/attributes'
+
+  # Set attr accessors
+  mattr_accessor :config
+
+  # Set config options
+  @@config = Config.new
+
+  # Setup module
+  def self.setup
+    yield config
+  end
+end
+
+# Include action view helpers
+if defined? ActionView::Base
+  ActionView::Base.send :include, SimpleAttribute::Helpers
 end
