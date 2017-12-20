b142add136e638b50311a11f102355a9f5780737
Olibia Tsati <olibia@hardpixel.eu>
2017-12-20 17:37:03 +0200

    add url_method option for association attribute

--- lib/simple_attribute/attributes/association.rb
+++ lib/simple_attribute/attributes/association.rb
@@ -16,10 +16,19 @@ module SimpleAttribute
         @options.fetch(:title, :id)
       end
 
+      # Link url method
+      def url_method
+        @options.fetch(:url, nil)
+      end
+
       # Render attribute
       def render_attribute
         label = value.try(label_method)
         title = value.try(title_method)
+
+        unless url_method.nil?
+          value = @context.send(url_method, @value)
+        end
 
         link_to label, value, html_options.merge(title: title)
       end
