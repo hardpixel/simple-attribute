df46420b8cf73973cb7293b64812f60096c62983
Olibia Tsati <olibia@hardpixel.eu>
2017-12-20 17:24:30 +0200

    better image attribute default

--- lib/simple_attribute/attributes/image.rb
+++ lib/simple_attribute/attributes/image.rb
@@ -6,6 +6,21 @@ module SimpleAttribute
         'image'
       end
 
+      # Get default value
+      def default_value
+        default = @options.fetch :default_value, nil
+        default = asset_path(default) rescue nil
+
+        default.nil? ? super : default
+      end
+
+      # Render default value
+      def render_default_value
+        if default_value.present?
+          image_tag default_value, html_options
+        end
+      end
+
       # Render attribute
       def render_attribute
         image_tag value, html_options
